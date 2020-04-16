a5a7ff965fb4083ada8967ad86bd720d9f8a2334
Daniil <brayzer208@gmail.com>
2020-04-16 10:52:45 +0300

    first commit

---
+++ MyProj/main.js
@@ -0,0 +1,43 @@
+'use strict';
+/*let second = 2;
+const pi = 3.14;
+ console.log(second);*/
+
+ let number = 5;
+ let string = "Hello!";
+ let sym = Symbol();
+ let boolean = true;
+ null; //когда ничего не существует!
+ undefined; //Когда существует , но не задано значение
+ let obj = {}; //Комплексный тип данных
+
+ console.log(4/0); //infinity
+ console.log('string' * 9); //NuN
+
+ /*let persone = {
+     name: 'John',
+     age: 25,
+     isMarried: false
+ };
+
+ console.log(persone["name"]);*/
+
+ /*let arr = ['plum.png', 'orange.png', 'apple.bmp']; //массив, нумерация с нуля
+ console.log(arr[0]);*/
+ /*let answer = confirm("Are you here?");
+ console.log(answer);*/
+
+
+ /*let text_inside = prompt('Есть ли вам 18 лет ?', );
+ console.log(text_inside);
+ console.log(typeof(text_inside));*/
+
+ /*console.log('arr' + '- object');*/
+
+ let incr = 10;
+ let decr = 10;
+
+ incr++;
+ decr--;
+ console.log(incr);
+ console.log(decr);
