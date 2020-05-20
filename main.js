b73a1b0f06cf129339905b08f3a91b2979ed6259
Daniil <brayzer208@gmail.com>
2020-05-20 08:34:51 +0300

    тема циклов

--- main.js
+++ main.js
@@ -6,10 +6,35 @@ answer[2] = prompt('Сколько вам лет?', '');
 
 document.write(answer); */
 
-const category = '';
+/* const category = '';
 
 console.log(`https://someurl.com/${category}/5`);
 
 const user = 'Daniil';
 
-console.log(`Hello, ${user}`);
+console.log(`Hello, ${user}`); */
+
+
+let num = 50;
+while(num < 55) {
+    console.log(num);
+    num++;
+}
+
+
+do {
+     console.log(num);
+     num++;
+}
+while(num < 55);
+
+
+for(let i = 1; i < 10; i++) {
+    if(i === 6) {
+        /* break; */// останавливает цикл
+        continue;// пропускает 6 и продолжает дальше
+    }
+
+    console.log(i);
+
+}
