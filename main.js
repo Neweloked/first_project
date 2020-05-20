a57e4b69bf4e0045485eee2ac82a546083c0ffe3
Daniil <brayzer208@gmail.com>
2020-05-20 12:57:37 +0300

    Тема функций

--- main.js
+++ main.js
@@ -15,7 +15,15 @@ const user = 'Daniil';
 console.log(`Hello, ${user}`); */
 
 
-let num = 50;
+
+
+
+
+
+
+
+
+/* let num = 50;
 while(num < 55) {
     console.log(num);
     num++;
@@ -31,10 +39,55 @@ while(num < 55);
 
 for(let i = 1; i < 10; i++) {
     if(i === 6) {
-        /* break; */// останавливает цикл
-        continue;// пропускает 6 и продолжает дальше
+        //break;
+        continue;
     }
 
     console.log(i);
 
+} */
+
+
+
+
+
+
+let num = 20;// глобальная переменная
+function showNewMessage(text) {
+    console.log(text);
+    let num = 20; //локальная переменная
+    num = 10; //Меняем глобальную переменную
 }
+
+
+showNewMessage('Hello World');
+console.log(num);
+
+
+/* function calc(a, b) {
+    return (a +  b);
+}
+
+console.log(calc(4,3)); */
+
+
+function ret() {
+    let num = 50;
+    return num;
+}
+
+const anotherNum = ret();
+console.log(anotherNum);
+
+
+const logger = function() {
+    console.log('Hellow!');
+};
+
+logger();
+
+
+const calc =(a, b) =>{
+    console.log('1');
+    return a + b;
+};
